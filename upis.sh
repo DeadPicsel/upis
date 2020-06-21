ed8da33e9450d97ad7efa7524aa50112ba11c990
Ry4nw <dannyboi1903@gmail.com>
2020-06-21 13:45:39 +0100

    Updated script for newer Ubuntu versions

--- upis.sh
+++ upis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo Ubuntu Post Installation Script by Danny Williamson
+echo Ubuntu Post Installation Script by Ry4nw
 
 # Make Directories
 mkdir ~/Apps
@@ -11,17 +11,19 @@ mkdir ~/VPN
 
 # Add 3rd Party Repositories Keys
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-wget -q -O - http://download.virtualbox.org/virtualbox/debian/oracle_vbox_2016.asc | sudo apt-key add -
+wget -q -O - https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q -O - https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 
 
 # Add 3rd Party Repositories
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-sudo sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian yakkety non-free contrib" >> /etc/apt/sources.list.d/virtualbox.org.list' 
+echo "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian eoan contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
 
 # Basic Update
 sudo apt-get -y --force-yes update
 sudo apt-get -y --force-yes upgrade
 sudo apt-get -y --force-yes dist-upgrade
+sudo apt-get install -y build-essential
 
 # Accessories Applications
 sudo apt-get install -y gedit
@@ -34,7 +36,6 @@ sudo apt-get install -y gimp
 sudo apt-get install -y google-chrome-stable
 sudo apt-get install -y deluge
 sudo apt-get install -y filezilla
-
 
 # Multimedia Applications
 sudo apt-get install -y audacious
@@ -49,9 +50,7 @@ sudo apt-get install -y virtualbox
 sudo adduser x vboxusers
 
 # VPN Setup
-wget -P ~/VPN https://btguard.com/btguard.ca.crt
-sudo apt-get install -y network-manager-openvpn-gnome
-sudo apt-get install -y network-manager-openvpn
+# REMOVED FOR NOW
 
 # User Input Needed
 sudo apt-get install -y ubuntu-restricted-extras
